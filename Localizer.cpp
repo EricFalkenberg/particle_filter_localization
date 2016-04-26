3dc1fc09412f001051fc07e415fad9020392fc70
Eric Falkenberg <exf4789@rit.edu>
2016-04-26 13:52:30 -0400

    Added a particle class to Localizer such that we can easily represent particles within our filter.

--- Localizer.cpp
+++ Localizer.cpp
@@ -1,5 +1,11 @@
 #include "Localizer.h"
 
+Particle::Particle(double x, double y, double theta) {
+    this->x = x;
+    this->y = y;
+    this->theta = theta;
+}
+
 Localizer::Localizer() {
     // Pass
 }
--- Localizer.h
+++ Localizer.h
@@ -1,7 +1,18 @@
 class Localizer {
-private:
-    // private members
-public:
-    // public members
-    Localizer();
+    private:
+        // private members
+    public:
+        // public members
+        Localizer();
+}
+
+class Particle {
+    private:
+        // private members
+    public:
+        // public members
+        double x;
+        double y;
+        double theta;
+        Particle(double x, double y, double theta);
 }
