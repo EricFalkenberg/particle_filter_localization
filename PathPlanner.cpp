fc1a8cb6eb026ce363a55c95856806d16d29a3a5
Ramsey Opp <threedliams@gmail.com>
2016-05-13 23:13:57 -0400

    added some more comments

--- PathPlanner.cpp
+++ PathPlanner.cpp
@@ -12,10 +12,16 @@ PathPlanner::PathPlanner(int8_t* MAP_DATA, int MAP_WIDTH, int MAP_HEIGHT, double
     this->MAP_RESOLUTION= MAP_RESOLUTION;
 }
 
+/**
+    gets the pixel value at x, y to check if it's pathable
+*/
 int8_t PathPlanner::get_pixel_val(double x, double y) {
     return MAP_DATA[(int)(MAP_HEIGHT / 2 + y/MAP_RESOLUTION) * MAP_WIDTH + (int)(x/MAP_RESOLUTION + MAP_WIDTH/2)];
 }
 
+/**
+    gets the optimal place to continue the path from
+*/
 geometry_msgs::PoseStamped PathPlanner::get_local_optimal(std::vector< geometry_msgs::PoseStamped >* poses, double start_x, double start_y, double goal_x, double goal_y) {
     geometry_msgs::PoseStamped min_p;
     double min_h = -1;
@@ -33,6 +39,10 @@ geometry_msgs::PoseStamped PathPlanner::get_local_optimal(std::vector< geometry_
     return min_p;
 }
 
+
+/**
+    gets the neighbors of the node, with a an allowed_error tolerance that determines how close the path can get to a wall
+*/
 std::vector< geometry_msgs::PoseStamped > PathPlanner::get_successors(geometry_msgs::PoseStamped node_current) {
     std::vector< geometry_msgs::PoseStamped > successors;
     geometry_msgs::PoseStamped n_node  = geometry_msgs::PoseStamped();
@@ -70,6 +80,9 @@ std::vector< geometry_msgs::PoseStamped > PathPlanner::get_successors(geometry_m
     return successors;
 }
 
+/**
+    check if a given node is in the list of nodes
+*/
 bool PathPlanner::target_in_list(std::vector< geometry_msgs::PoseStamped > list, geometry_msgs::PoseStamped target) {
     for (int idx = 0; idx < list.size(); idx++) {
         if (list[idx].pose.position.x == target.pose.position.x && list[idx].pose.position.y == target.pose.position.y) {
@@ -79,6 +92,9 @@ bool PathPlanner::target_in_list(std::vector< geometry_msgs::PoseStamped > list,
     return false;
 }
 
+/**
+    removes a given node from the list of nodes
+*/
 std::vector< geometry_msgs::PoseStamped > PathPlanner::remove_target_from_list(std::vector< geometry_msgs::PoseStamped > list, geometry_msgs::PoseStamped target) {
     int toRemove = -1;
     for (int idx = 0; idx < list.size(); idx++) {
@@ -90,6 +106,9 @@ std::vector< geometry_msgs::PoseStamped > PathPlanner::remove_target_from_list(s
     return list;
 }
 
+/**
+    finds the parent node for a given node in the path.
+*/
 geometry_msgs::PoseStamped PathPlanner::find_parent(std::vector< std::vector< geometry_msgs::PoseStamped > > history, geometry_msgs::PoseStamped target) {
     for (int idx = 0; idx < history[0].size(); idx++) {
         if (history[0][idx].pose.position.x == target.pose.position.x && history[0][idx].pose.position.y == target.pose.position.y) {
@@ -101,6 +120,9 @@ geometry_msgs::PoseStamped PathPlanner::find_parent(std::vector< std::vector< ge
     return p;
 }
 
+/**
+    calculates the slope between two points (x0, y0) and (x1, y1)
+*/
 double slope(double x0, double y0, double x1, double y1){
     double dx = double(x1-x0);
 
@@ -112,10 +134,17 @@ double slope(double x0, double y0, double x1, double y1){
     }
 }
 
+/**
+    gives the distance between 2 points (x0, y0) and (x1, y1)
+*/
 double distBetween(double x0, double y0, double x1, double y1){
     return pow(pow(x1 - x0, 2) + pow(y1 - y0, 2), .5);
 }
 
+
+/**
+compresses the path to improve execution speed. eliminates intermediate points that don't very the slope too much
+*/
 nav_msgs::Path PathPlanner::compression(nav_msgs::Path path){
 
     int i = 1;
